1ea23eba381599ab8aa01d52d48ef50ba422e0ed
mouryachandan <ck43663@gmail.com>
2026-07-13 03:17:05 +0530

    done

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import { showAppError } from "./appAlert";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://interview-cochhh.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://interview-cochhh.onrender.com/api",
   
 });
 
