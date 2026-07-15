c55aeb3c86793b2f07d1da19b18fb0fc1cc14c5c
mouryachandan <ck43663@gmail.com>
2026-07-16 00:54:05 +0530

    done

--- adminfrontend/src/utils/api.js
+++ adminfrontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://admiminterview-coch.onrender.comcd/api/admin',
+  baseURL: 'https://admiminterview-coch.onrender.com/api/admin',
 });
 
 // Add a request interceptor to attach the admin token
