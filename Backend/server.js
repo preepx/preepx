fa0f26c6b3f639b9cbe72e41f4a2eeb6d9663dac
mouryachandan <ck43663@gmail.com>
2026-07-26 21:36:14 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -17,8 +17,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://preepx.in",
-  "https://www.preepx.in"
+
 ].filter(Boolean);
 
 app.use(cors({
