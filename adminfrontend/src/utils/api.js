782401feafc6e19664a5338aa40a4130036b4330
mouryachandan <ck43663@gmail.com>
2026-07-28 16:38:46 +0530

    jj

--- adminfrontend/src/utils/api.js
+++ adminfrontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://admiminterview-coch.onrender.com/api/admin',
+  baseURL: 'https://admiminterview-coch.onrender.com/api/admin..',
 });  //https://admiminterview-coch.onrender.com  local http://localhost:5000/api/admin
 
 // Add a request interceptor to attach the admin token
