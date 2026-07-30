2f2a1d44b6825e96a60cd03dcc78e43fbe0e0445
mouryachandan <ck43663@gmail.com>
2026-07-30 16:59:22 +0530

    done

--- Backend/middleware/apiKeyAuth.js
+++ Backend/middleware/apiKeyAuth.js
@@ -1,6 +1,6 @@
 const requireApiKey = (req, res, next) => {
   const clientKey = req.headers['x-api-key'];
-  const serverKey = process.env.API_SECRET_KEY || "PreepX_Secret_Key_12345";
+  const serverKey = process.env.API_SECRET_KEY || "PreepXSecretKey8969675472";
 
   if (!clientKey || clientKey !== serverKey) {
     return res.status(403).json({ message: "Access Denied: Invalid or missing API Key" });
