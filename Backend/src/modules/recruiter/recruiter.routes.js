0a34a541be3eecc12dd7384bdfe0057a79d34cc3
mouryachandan <ck43663@gmail.com>
2026-08-11 19:24:43 +0530

    fix rate limiter path in recruiter routes

--- Backend/src/modules/recruiter/recruiter.routes.js
+++ Backend/src/modules/recruiter/recruiter.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const recruiterController = require('./recruiter.controller');
-const { authLimiter } = require('../../../common/middleware/rateLimiter');
+const { authLimiter } = require('../../common/middleware/rateLimiter');
 
 router.post('/register', authLimiter, recruiterController.registerRecruiter);
 router.post('/login', authLimiter, recruiterController.loginRecruiter);
