0bad4cb7638f79ac32e14b65c2676c96da39bcf0
mouryachandan <ck43663@gmail.com>
2026-08-17 18:33:41 +0530

    done

--- frontend/src/utils/announcement.js
+++ frontend/src/utils/announcement.js
@@ -5,7 +5,7 @@ export function triggerAnnouncement() {
 }
 
 export function shouldShowAnnouncement() {
-  return sessionStorage.getItem(SESSION_KEY) === "1";
+  return sessionStorage.getItem(SESSION_KEY) === "2";
 }
 
 export function dismissAnnouncement() {
